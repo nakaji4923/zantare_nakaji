fc6369744f9d394af4ff47987ee5de7cbaf99fb4
nakaji <nakaji4923@gmail.com>
2010-10-04 17:21:40 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ZantareNakaji::Application.config.secret_token = '3911287bb12a059eb01ffdce313700ede1a78461c5731a9c07b9acf21ce728a91629c2b808f8b6ab9d06b77f66643c319708836b291cf3138d90155270dad3f5'
